317edb3bb9959094a74e26c032a9d80c13700994
saidhij <saidhij@users.noreply.github.com>
2020-08-16 13:48:57 +0000

    Update __manifest__.py

--- l10n_ma_hr_payroll_10Basic/__manifest__.py
+++ l10n_ma_hr_payroll_10Basic/__manifest__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Odoo, Open Source Management Solution
-#    Copyright (C) 2016 rhfree (<http://rhfree.com>).
+#    Copyright (C) 2016 rhfree (<http://paiesoft.pro>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -21,8 +21,8 @@
 {
     'name': 'Morocco-payroll Basic',
     'category': 'Human Resources',
-    'author': 'rhfree.com',
-    'website': 'http://rhfree.com',
+    'author': 'paiesoft.pro',
+    'website': 'http://paiesoft.pro',
     "license": "AGPL-3",
     'version': '1.3Basic',
     'depends': ['hr_payroll'],
@@ -38,7 +38,7 @@
 	        - Seniority ( anciennété) and all other advantages:
         	- CIMR and private health insurance like  AXA 
 			- Nice looking payslip
-			- Legal reports ( etat 9421 ) ...
+			- Legal reports ( etat 9421 xml and  etat CNSS ) ...
     - Important: you need to fill the wage amount for the employee in the contract and chose moroccan payroll from the structure field.
     """,
     'data': [
